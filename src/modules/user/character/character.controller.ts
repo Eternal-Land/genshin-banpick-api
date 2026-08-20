eda025ca5b51f2bb50d15cf87c75eaa7213f7e06
Le Thanh Long <longlt201203@gmail.com>
2026-08-20 02:43:02 +0000

    feat: update search characters

--- src/modules/user/character/character.controller.ts
+++ src/modules/user/character/character.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get } from "@nestjs/common";
-import { ApiBearerAuth } from "@nestjs/swagger";
+import { Controller, Get, Query } from "@nestjs/common";
+import { ApiBearerAuth, ApiQuery } from "@nestjs/swagger";
 import { BaseApiResponse, SwaggerBaseApiResponse } from "@utils";
 import { CharacterResponse } from "@modules/admin/character/dto";
 import { UserCharacterService } from "./character.service";
@@ -8,6 +8,19 @@ import { UserCharacterService } from "./character.service";
 @ApiBearerAuth()
 export class UserCharacterController {
 	constructor(private readonly characterService: UserCharacterService) {}
+
+	@Get("search")
+	@SwaggerBaseApiResponse(CharacterResponse, { isArray: true })
+	@ApiQuery({
+		name: "query",
+		required: false,
+		type: String,
+		description: "Search query for character name or key",
+	})
+	async searchCharacters(@Query("query") query?: string) {
+		const characters = await this.characterService.searchCharacters(query);
+		return BaseApiResponse.success(CharacterResponse.fromEntities(characters));
+	}
 
 	@Get()
 	@SwaggerBaseApiResponse(CharacterResponse, { isArray: true })
